449a0f46ab1e647fa8d93826f32a363fff38352f
Ota(Dominik) Frey <dollonthefox@gmail.com>
2024-05-23 14:01:50 +0200

    removed examples and build file.

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,3 @@
-workspace "LiveLoveLog"
-    architecture "x64"
-    startproject "example"
-    toolset "v142"
-    configurations 
-    {
-        "Debug",
-        "Release"
-    }
-
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 project "LibLogLaugh"
@@ -24,11 +14,6 @@ project "LibLogLaugh"
     {
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp"
-    }
-
-    defines
-    {
-
     }
 
     includedirs
@@ -51,42 +36,3 @@ project "LibLogLaugh"
         runtime "Release"
         symbols "on"
 
-
-project "example"
-    location "example"
-    kind "ConsoleApp"
-    language "c++"
-    cppdialect "C++20"
-    staticruntime "on"
-
-
-	targetdir ("bin/" .. "/%{prj.name}")
-	objdir ("bin/int/" ..  "/%{prj.name}")
-    
-    files
-    {
-        "%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "LibLogLaugh/src"
-
-    }
-    links
-    {
-        "LibLogLaugh"
-    }
-
-
-    filter "system:windows"
-    systemversion "latest"
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        runtime "Release"
-        symbols "on"
